6513806fe58cd8948fb4fd2b27977a4ea63b082d
manhbuithe <manhbui@remitano.com>
2023-01-14 13:52:13 +0700

    Update bigdecimal

--- lib/gt-collection-calculator.rb
+++ lib/gt-collection-calculator.rb
@@ -1,11 +1,11 @@
 require 'bigdecimal'
 
 class GtCollectionCalculator
-  MAX_AMOUNT_THAT_USE_PERCENT = BigDecimal.new(2000)/0.015
+  MAX_AMOUNT_THAT_USE_PERCENT = BigDecimal(2000)/0.015
   MAX_DEBIT_AMOUNT_THAT_USE_PERCENT = MAX_AMOUNT_THAT_USE_PERCENT * 1.015
 
   def self.credit2request(credit_amount)
-    debit_amount = BigDecimal.new(credit_amount) + 105
+    debit_amount = BigDecimal(credit_amount) + 105
     if debit_amount > MAX_AMOUNT_THAT_USE_PERCENT
       debit_amount - 2000
     else
@@ -14,7 +14,7 @@ class GtCollectionCalculator
   end
 
   def self.request2credit(request_amount)
-    fee = [BigDecimal.new(request_amount) * 0.015, 2000].min
+    fee = [BigDecimal(request_amount) * 0.015, 2000].min
     request_amount + fee - 105
   end
 end
--- lib/gt-collection-calculator/version.rb
+++ lib/gt-collection-calculator/version.rb
@@ -1,3 +1,3 @@
 class GtCollectionCalculator
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
